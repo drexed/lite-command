169f6677a5966e448b5168012eb8a9341bd465d1
Juan Gomez <drexed@users.noreply.github.com>
2024-09-24 00:04:32 -0400

    Remove extra spacing

--- lib/lite/command/base.rb
+++ lib/lite/command/base.rb
@@ -12,7 +12,7 @@ module Lite
       attr_reader :context, :metadata
 
       def initialize(context = {})
-        @context  = Lite::Command::Construct.build(context)
+        @context = Lite::Command::Construct.build(context)
         @metadata = Lite::Command::Construct.init
       end
 
