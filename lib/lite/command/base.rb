a04c72bec9d8b7ec8aa8b739ac239d8fc770b0a5
Juan Gomez <drexed@users.noreply.github.com>
2024-09-26 11:55:38 -0400

    Improve error eval

--- lib/lite/command/base.rb
+++ lib/lite/command/base.rb
@@ -5,25 +5,30 @@ module Lite
     class Base
 
       def self.inherited(base)
+        super
+
         base.extend Forwardable
         base.include Lite::Command::Internals::Traceable
         base.include Lite::Command::Internals::Callable
         base.include Lite::Command::Internals::Executable
         base.include Lite::Command::Internals::Resultable
-        base.class_eval do
+
+        base.class_eval <<-RUBY, __FILE__, __LINE__ + 1
           # eg: Users::ResetPassword::Fault
-          eval("class #{base}::Fault < Lite::Command::Fault; end")
+          class #{base}::Fault < Lite::Command::Fault; end
+        RUBY
 
-          FAULTS.each do |call_fault|
-            fault_method = call_fault.downcase.capitalize
+        FAULTS.each do |call_fault|
+          fault_method = call_fault.downcase.capitalize
 
+          base.class_eval <<-RUBY, __FILE__, __LINE__ + 1
             # eg: Users::ResetPassword::Noop < Users::ResetPassword::Fault
-            eval("class #{base}::#{fault_method} < #{base}::Fault; end")
-          end
-
-          attr_reader :context, :metadata
+            class #{base}::#{fault_method} < #{base}::Fault; end
+          RUBY
         end
       end
+
+      attr_reader :context, :metadata
 
       def initialize(context = {})
         @context = Lite::Command::Construct.build(context)
