77380c03d99f6a83f2d3fdc7ebb7217278548718
Juan Gomez <drexed@users.noreply.github.com>
2024-10-05 13:49:34 -0400

    Clean up context specs

--- lib/lite/command.rb
+++ lib/lite/command.rb
@@ -3,14 +3,14 @@
 require "generators/rails/command_generator" if defined?(Rails::Generators)
 
 require "lite/command/version"
+require "lite/command/context"
 require "lite/command/attribute"
 require "lite/command/attribute_validator"
+require "lite/command/fault"
 
 require "lite/command/internals/context"
 require "lite/command/internals/call"
 require "lite/command/internals/execute"
 require "lite/command/internals/fault"
 require "lite/command/internals/result"
-require "lite/command/fault"
-require "lite/command/context"
 require "lite/command/base"
--- spec/lite/command/context_spec.rb
+++ spec/lite/command/context_spec.rb
@@ -3,23 +3,41 @@
 require "spec_helper"
 
 RSpec.describe Lite::Command::Context do
-  subject(:context) { described_class.build(params) }
+  subject(:ctx) { described_class.build(params) }
 
   let(:params) do
     { attr_one: "val_one" }
   end
 
   describe ".build" do
-    it "returns same context" do
-      other_context = described_class.build(context)
-      expect(context.object_id).to eq(other_context.object_id)
+    context "when context is not frozen" do
+      it "returns same context" do
+        other_ctx = described_class.build(ctx)
+
+        expect(ctx.object_id).to eq(other_ctx.object_id)
+      end
+    end
+
+    context "when context is frozen" do
+      it "returns a new context" do
+        other_ctx = described_class.build(ctx.freeze)
+
+        expect(ctx.object_id).not_to eq(other_ctx.object_id)
+      end
     end
   end
 
   describe ".merge!" do
     it "adds new key value pair to the context" do
-      context[:attr_two] = "val_two"
-      expect(context.attr_two).to eq("val_two")
+      ctx.merge!(attr_two: "val_two", attr_three: "val_three")
+
+      expect(ctx.to_h).to eq(
+        {
+          attr_one: "val_one",
+          attr_two: "val_two",
+          attr_three: "val_three"
+        }
+      )
     end
   end
 end
