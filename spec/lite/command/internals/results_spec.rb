2e79cd570b5069105ecffb5da099e2838bfcb796
Juan Gomez <drexed@users.noreply.github.com>
2024-10-15 10:53:23 -0400

    Add results specs

--- spec/lite/command/internals/results_spec.rb
+++ spec/lite/command/internals/results_spec.rb
@@ -3,31 +3,56 @@
 require "spec_helper"
 
 RSpec.describe Lite::Command::Internals::Results do
-  # config.before do
-  #   [
-  #     ValidationTokenCommand
-  #   ].each do |klass|
-  #     allow_any_instance_of(klass).to receive(:freeze_execution_objects).and_return(true)
-  #   end
-  # end
+  subject(:command) { EmailValidatorCommand.call(user:, simulate_token_collision:) }
 
-  # context "when noop" do
-  #   let(:command_class) { NoopCommand }
+  let(:user) { User.new }
+  let(:simulate_token_collision) { false }
 
-  #   it "returns a noop status" do
-  #     expect(command.results).not_to be_empty
-  #     expect(command.to_hash).to eq(
-  #       index: 1,
-  #       cmd_id: "018c2b95-b764-7615-a924-cc5b910ed1e5",
-  #       command: "NoopCommand",
-  #       outcome: "noop",
-  #       state: "interrupted",
-  #       status: "noop",
-  #       reason: "[!] command stopped due to noop",
-  #       caused_by: 1,
-  #       thrown_by: 1,
-  #       runtime: 0.0123
-  #     )
-  #   end
-  # end
+  before do
+    [
+      EmailValidatorCommand,
+      ValidationTokenCommand
+    ].each do |klass|
+      allow_any_instance_of(klass).to receive(:freeze_execution_objects).and_return(true)
+      allow_any_instance_of(klass).to receive(:cmd_id).and_return("018c2b95-b764-7615-a924-cc5b910ed1e5")
+      allow_any_instance_of(klass).to receive(:runtime).and_return(0.0123)
+    end
+  end
+
+  context "when noop" do
+    context "without fault" do
+      it "returns correct data" do
+        expect(command.results.size).to eq(2)
+        expect(command.to_hash).to eq(
+          index: 1,
+          cmd_id: "018c2b95-b764-7615-a924-cc5b910ed1e5",
+          command: "EmailValidatorCommand",
+          outcome: "success",
+          state: "complete",
+          status: "success",
+          runtime: 0.0123
+        )
+      end
+    end
+
+    context "with fault" do
+      let(:simulate_token_collision) { true }
+
+      it "returns correct data" do
+        expect(command.results.size).to eq(2)
+        expect(command.to_hash).to eq(
+          index: 1,
+          cmd_id: "018c2b95-b764-7615-a924-cc5b910ed1e5",
+          command: "EmailValidatorCommand",
+          outcome: "interrupted",
+          state: "interrupted",
+          status: "failure",
+          reason: "Validation token already exists",
+          caused_by: 2,
+          thrown_by: 2,
+          runtime: 0.0123
+        )
+      end
+    end
+  end
 end
